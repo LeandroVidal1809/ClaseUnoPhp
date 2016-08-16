0d7503e62a1fa0ba94b9fc781c30528220ee5806
Leandro Vidal <leandrovidal92@gmail.com>
2016-08-16 20:24:45 -0300

    Se realizo la primer prueba en php

---
+++ index.php
@@ -0,0 +1,20 @@
+<html>
+<head>
+	<title>Vidal</title>
+</head>
+<body>
+		<h1>Hola Mundo</h1>
+
+		<?php
+			$Numero=10;
+			echo "El Numero es: $Numero ";
+
+			//si pongo solo una comilla , la frase saldra literalmente , no mostrara la variable
+			echo "<br>";
+			//---------------br genera salto de linea'
+			echo "Hola PHP";
+			echo"<h2>  Hola PHP </h2>";
+		?>
+
+</body>
+</html>
